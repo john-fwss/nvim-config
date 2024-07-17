d52fb647656361e2b651033b7c379a73eed92142
john <john@fwss.us>
2024-07-16 20:08:35 -0400

    add cursor line and column number instead of percentage

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,24 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "doomchad",
+  theme = "doomchad",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  statusline = {
+    modules = {
+      cursor = function()
+        return "%#St_pos_sep#" .. "%#St_pos_icon# Ln %l, Col %c  "
+      end,
+    },
+  },
+
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
